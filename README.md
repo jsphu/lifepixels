6950bdb0df5bb20c00740eea59fad8b1ba544e9b
jsphu <yunal22@ogr.iu.edu.tr>
2026-03-03 13:56:21 +0300

    Fix relative path issues

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Lifepixels
+---
 ## Turn your favorite animal into Github Pixel Graph
 
 ### Search Query: "Naja" (Cobra)
+---
 ![organism](./organism.svg)
 
----
-
 ### Start using with:
+---
 ```shell
-yarn dev "Mus musculus" "./organism.svg"
+yarn dev --query "Mus musculus" --output "./organism.svg"
 ```
+
 #### Usage
+---
 ```shell
-yarn dev [ORG_NAME] [OUT_DIR]
+yarn dev [[--gridSize | -g] GRIDSIZE] [--query | -q] QUERY [--output | -o] OUTPUT
 ```
--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,53 @@ import { EOLProvider } from './providers/eol';
 import { pixelate } from './core/engine';
 import { generateSVG } from './core/renderer';
 import fs from 'fs';
+import path from 'path';
+import { parseArgs } from 'util';
 
 async function run() {
-    const query = process.argv[2] || ""; // Fallback
-    const output = process.argv[3] || "./dist/organism.svg";
-    const provider = new EOLProvider();
+    const usage: string = "usage: [[--gridSize | -g] GRIDSIZE] [--query | -q] QUERY [--output | -o] OUTPUT";
+    const config = {
+        options: {
+            query: { short: 'q', type: 'string', },
+            output: { short: 'o', type: 'string' },
+            gridSize: { short: 'g', type: 'string', default: '30' },
+        },
+        allowPositionals: true,
+    } as const;
+    const { values, positionals } = parseArgs(config);
+    const query: string | undefined = values.query ?? positionals[0];
+    const output: string | undefined = values.output ?? positionals[1];
+    const gridSize: number = parseInt(values.gridSize) ?? 30; // Generate a 30x30 grid for a compact profile look
+    const provider: EOLProvider = new EOLProvider();
 
     if (!query) {
         console.error("Query should be a scientific name of animal.")
-        console.error("usage: dev QUERY OUTPUT")
-        process.exit(1);
+        console.error(usage)
+        process.exit(2);
+    } else if (!output) {
+        console.error("Output path required.")
+        console.error(usage)
+        process.exit(2);
     }
-
-        console.log(`Searching for: ${query}...`);
+    console.log(`Searching for: ${query}...`);
     const organism = await provider.fetchOrganismImage(query);
 
     if (!organism) {
-        console.error("No image found for that organism.");
+        console.error(`No image found for ${query}.`);
+        console.error("Either organism not found or api could not found an image.")
         process.exit(1);
     }
 
-    // Generate a 30x30 grid for a compact profile look
-    const gridSize = 30;
     const pixels = await pixelate(organism.buffer, gridSize);
     const svg = generateSVG(pixels, gridSize, gridSize);
 
-    let output_dir
-    if (output) {
-        output_dir = output.substring(output.lastIndexOf('/') + 1, output.length - 1)
-    } else {
-        console.error("An output file path required.")
-        process.exit(1)
-    }
+    const absPath = path.resolve(output); // Get the absolute path
+    const outputDir = path.dirname(absPath); // Get the directory of path
     // Ensure the output directory exists
-    if (!fs.existsSync(output_dir)) fs.mkdirSync(output_dir);
+    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, {recursive: true});
 
-    fs.writeFileSync(output, svg);
-    console.log(`Success! Saved to ${output}`);
+    fs.writeFileSync(absPath, svg);
+    console.log(`Success! Saved to ${absPath}`);
 }
 
 run();
